7fac78df92c4a7d2affa3364a84004a529cd8fee
CurtTimson <curt@live.co.uk>
2017-03-27 19:21:33 +0100

    Refactor publish and deploy tasks into seperate JS files

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,6 @@
 var gulp = require("gulp");
-var ghPages = require('gulp-gh-pages');
 
-gulp.task('publish', function(){
-  return gulp.src('./app/**/**')
-    .pipe(gulp.dest('dist'));
-});
 
-gulp.task('deploy', function() {
-  return gulp.src('./dist/**/*')
-    .pipe(ghPages());
-});
+var requireDir = require('require-dir');
+
+requireDir('./gulp-tasks');

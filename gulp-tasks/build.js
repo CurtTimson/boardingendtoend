a4f56391dc86d0e953f9d05b58926b7d3b08237e
Curtis Timson <curt@live.co.uk>
2018-02-11 17:21:23 +0000

    :heavy_plus_sign: Add hugo theme dependency

--- gulp-tasks/build.js
+++ gulp-tasks/build.js
@@ -1,26 +1,8 @@
 var gulp = require("gulp");
-var runSequence = require('run-sequence');
-var rename = require('gulp-rename');
 
-var baseDir = "holding-page";
+gulp.task('copy-npm-files', ['copy-npm-files-theme']);
 
-gulp.task('build-files', function(){
-  return gulp.src('./' + baseDir + '/**/**')
-    .pipe(gulp.dest('dist'));
-});
-
-gulp.task('build-config', function() {
-
-  var file = "production"
-
-  console.log("Config: " + file);
-
-  return gulp.src('config/' + file + '.js')
-             .pipe(rename('config.js'))
-             .pipe(gulp.dest('dist/scripts'));
-});
-
-
-gulp.task('build', function(){
-  runSequence('build-files', 'build-config');
+gulp.task('copy-npm-files-theme', () => {
+  return gulp.src(['./node_modules/hugo-theme-dopetrope/**/**.**'])
+        .pipe(gulp.dest('./themes/dopetrope'));
 });
